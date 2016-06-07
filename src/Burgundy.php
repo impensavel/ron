81ed1d61be5549739e63bbb81676dda6fa48a078
Quetzy Garcia <quetzyg@altek.org>
2016-06-08 00:22:49 +0100

    feat(Burgundy): class refactor / decouple HTTP client by using HTTPlug

--- src/Burgundy.php
+++ src/Burgundy.php
@@ -3,7 +3,7 @@
  * This file is part of the Ron library.
  *
  * @author     Quetzy Garcia <quetzyg@impensavel.com>
- * @copyright  2015
+ * @copyright  2015-2016
  *
  * For the full copyright and license information,
  * please view the LICENSE.md file that was distributed
@@ -15,40 +15,51 @@ namespace Impensavel\Ron;
 use DOMNodeList;
 use Exception;
 
-use GuzzleHttp\Client;
+use Http\Client\HttpClient;
+use Http\Message\MessageFactory;
 use Impensavel\Essence\EssenceException;
-use Impensavel\Essence\XMLEssence;
+use Impensavel\Essence\XML;
 
 class Burgundy
 {
     /**
-     * XML Essence
+     * XML parser
      *
      * @access  protected
-     * @var     XMLEssence
+     * @var     XML
      */
-    protected $essence;
+    protected $parser;
 
     /**
-     * Guzzle HTTP client
+     * HTTP client
      *
      * @access  protected
-     * @var     \GuzzleHttp\Client
+     * @var     HttpClient
      */
-    protected $http;
+    protected $client;
+
+    /**
+     * Message factory
+     *
+     * @access  protected
+     * @var     \Http\Message\MessageFactory
+     */
+    protected $message;
 
     /**
      * Burgundy constructor
      *
      * @access  public
-     * @param   XMLEssence         $essence XML Essence
-     * @param   \GuzzleHttp\Client $http    Guzzle HTTP client
+     * @param   XML            $parser  XML parser
+     * @param   HttpClient     $client  HTTP client
+     * @param   MessageFactory $message Message factory
      * @return  Burgundy
      */
-    public function __construct(XMLEssence $essence, Client $http)
+    public function __construct(XML $parser, HttpClient $client = null, MessageFactory $message = null)
     {
-        $this->essence = $essence;
-        $this->http = $http;
+        $this->parser = $parser;
+        $this->client = $client;
+        $this->message = $message;
     }
 
     /**
@@ -56,11 +67,15 @@ class Burgundy
      *
      * @static
      * @access  protected
-     * @param   array  $spec Feed Specification
+     * @param   array  $spec Feed specification
      * @return  array
      */
     protected static function normalise(array $spec)
     {
+        if (! isset($spec['name'])) {
+            throw new RonException('Feed specification name not set');
+        }
+
         return array_replace_recursive($spec, [
             'map'        => [],
             'namespaces' => [],
@@ -68,15 +83,15 @@ class Burgundy
     }
 
     /**
-     * Create an XML Essence object
+     * Create an XML parser
      *
      * @static
      * @access  protected
      * @param   array  $specs Feed specifications
      * @throws  RonException
-     * @return  XMLEssence
+     * @return  XML
      */
-    protected static function essentialise(array $specs)
+    protected static function createParser(array $specs)
     {
         $config = [];
         $namespaces = [];
@@ -84,27 +99,26 @@ class Burgundy
         foreach ($specs as $xpath => $spec) {
             $spec = static::normalise($spec);
 
-            // compile element map and data handler
+            // Compile Element map and data handler
             $config[$xpath] = [
                 'map'     => $spec['map'],
-                'handler' => function ($element, array $properties, &$stories)
-                {
+                'handler' => function ($element, array $properties, &$stories) use ($spec) {
                     foreach ($properties as $name => $value) {
                         if ($value instanceof DOMNodeList) {
-                            $properties[$name] = XMLEssence::DOMNodeListToArray($value);
+                            $properties[$name] = XML::DOMNodeListToArray($value);
                         }
                     }
 
-                    $stories[] = new Story($properties);
+                    $stories[] = new Story($spec['name'], $properties);
                 },
             ];
 
-            // compile namespaces
+            // Compile namespaces
             $namespaces = array_merge($namespaces, $spec['namespaces']);
         }
 
         try {
-            return new XMLEssence($config, $namespaces);
+            return new XML($config, $namespaces);
         } catch (EssenceException $e) {
             throw new RonException($e->getMessage(), $e->getCode(), $e);
         }
@@ -115,39 +129,24 @@ class Burgundy
      *
      * @static
      * @access  public
-     * @param   array  $config Configurations
+     * @param   HttpClient     $client
+     * @param   MessageFactory $message
+     * @param   array          $specs   Feed specifications
      * @throws  RonException
      * @return  Burgundy
      */
-    public static function create(array $config = [])
+    public static function create(HttpClient $client = null, MessageFactory $message = null, array $specs = [])
     {
-        // configuration defaults
-        $config = array_replace_recursive([
-            'specs' => [
-                'feed/entry'       => require 'specs/Atom.php',
-                'rss/channel/item' => require 'specs/RSS.php',
-                'rdf:RDF/item'     => require 'specs/RDF.php',
-            ],
-            'http'  => [
-                'defaults' => [
-                    'headers' => [
-                        'User-Agent' => 'Burgundy/1.0',
-                    ],
-                ],
-            ],
-        ], $config, [
-            'http' => [
-                'defaults' => [
-                    'exceptions' => false,
-                ],
-            ],
-        ]);
+        // Default Feed specifications
+        $specs = array_replace_recursive([
+            'feed/entry'       => require 'specs/Atom.php',
+            'rss/channel/item' => require 'specs/RSS.php',
+            'rdf:RDF/item'     => require 'specs/RDF.php',
+        ], $specs);
 
-        $essence = static::essentialise($config['specs']);
+        $parser = static::createParser($specs);
 
-        $http = new Client($config['http']);
-
-        return new static($essence, $http);
+        return new static($parser, $client, $message);
     }
 
     /**
@@ -161,24 +160,32 @@ class Burgundy
      */
     public function read($input, array $options = [])
     {
-        $options = array_replace_recursive([
-            'options'  => LIBXML_PARSEHUGE|LIBXML_DTDLOAD|LIBXML_NSCLEAN,
-        ], $options);
-
-        try {
-            $stories = [];
-
-            // fetch the content if the input is a URL
-            if (filter_var($input, FILTER_VALIDATE_URL) !== false) {
-                $response = $this->http->get($input);
-
-                $input = (string) $response->getBody();
+        // Fetch input from a URL
+        if ($url = filter_var($input, FILTER_VALIDATE_URL)) {
+            if ($this->client === null || $this->message === null) {
+                throw new RonException('HttpClient and MessageFactory required to fetch data from a URL');
             }
 
-            $this->essence->extract($input, $options, $stories);
+            try {
+                $request = $this->message->createRequest('GET', $url);
+                $response = $this->client->sendRequest($request);
+                $input = $response->getBody()->getContents();
+            } catch (Exception $e) {
+                throw new RonException($e->getMessage(), $e->getCode(), $e);
+            }
+        }
+
+        try {
+            $options = array_replace_recursive([
+                'options'  => LIBXML_PARSEHUGE|LIBXML_DTDLOAD|LIBXML_NSCLEAN,
+            ], $options);
+
+            $stories = [];
+
+            $this->parser->extract($input, $options, $stories);
 
             return $stories;
-        } catch (Exception $e) {
+        } catch (EssenceException $e) {
             throw new RonException($e->getMessage(), $e->getCode(), $e);
         }
     }
